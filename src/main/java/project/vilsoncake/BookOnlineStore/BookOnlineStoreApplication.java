5350d747535f1f07a914207bbda6e21a0450f7d5
ViLsonCake <114938803+ViLsonCake@users.noreply.github.com>
2023-07-12 16:21:47 +0200

    init project

---
+++ src/main/java/project/vilsoncake/BookOnlineStore/BookOnlineStoreApplication.java
@@ -0,0 +1,13 @@
+package project.vilsoncake.BookOnlineStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookOnlineStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookOnlineStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/vilsoncake/BookOnlineStore/BookOnlineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package project.vilsoncake.BookOnlineStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookOnlineStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
